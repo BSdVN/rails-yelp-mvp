7a0bc6524612c3efed898e834cb6bad84a082f53
Beltran Sainz de Vicuna Nowack <gorthor@gmail.com>
2018-11-07 17:17:35 +0100

    remember to rake, missing db:seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  "restaurants",     to: "restaurants#index"
+
+  get  "restaurants/new", to: "restaurants#new"
+  post "restaurants",     to: "restaurants#create"
+
+  get  "restaurants/:id", to: "restaurants#show"
+
+  resources :restaurants, only: [] do
+    resources :reviews, only: %i[new create]
+  end
+  # get  "reviews/new", to: "reviews#new"
+  # post "reviews",     to: "reviews#create"
 end
